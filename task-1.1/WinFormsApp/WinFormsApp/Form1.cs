f13dfcee0f5ddaa37b93b76a3c683c0fcde41e44
kktrnrl <kateryna.orel.student@knu.ua>
2023-10-03 18:15:58 +0300

    finished task 1.1

---
+++ task-1.1/WinFormsApp/WinFormsApp/Form1.cs
@@ -0,0 +1,27 @@
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "Hello World!";
+        }
+    }
+}
