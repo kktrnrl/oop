8e071fb5199779350d87c8a4c6753182ffb0c5b6
kktrnrl <kateryna.orel.student@knu.ua>
2023-10-04 02:18:15 +0300

    finished task 1.2

---
+++ task1.2/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnContinueClicked(object sender, EventArgs e)
+        {
+            OnEntryCompleted(entry, e);
+        }
+
+        private void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+
+            bool checkNumber = int.TryParse(text, out int number);
+
+            if (checkNumber == true)
+            {
+                VerifyTxt.Text = ("Ви ввели число N");
+            }
+            else
+            {
+                VerifyTxt.Text = ("error");
+            }
+        }
+
+        private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+            string myText = entry.Text;
+        }
+    }
+}
